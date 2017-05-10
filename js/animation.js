c7e46e02a20260080c6dcd2f49013dc309f70084
MateuszBieniek92 <bieniek.mateusz92@gmail.com>
2017-05-10 19:10:24 +0200

    Changing calculators background in menu.

--- js/animation.js
+++ js/animation.js
@@ -1,6 +1,103 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    console.log('dom!');
+    const bmiForm = document.querySelector('.bmi');
+    const caloriesForm = document.querySelector('.calories');
+    const basicMetaForm = document.querySelector('.basicMetabolism');
+    const totalMetaForm = document.querySelector('.totalMetabolism');
+
+    const firstMenuBtn = document.querySelector('.btnOne');
+    const secondMenuBtn = document.querySelector('.btnTwo');
+    const thirdMenuBtn = document.querySelector('.btnThree');
+    const fourthMenuBtn = document.querySelector('.btnFour');
+
+    console.log(bmiForm, caloriesForm);
+
+
+    function init() {
+        bmiForm.style.display = 'block';
+        caloriesForm.style.display = 'none';
+        basicMetaForm.style.display = 'none';
+        totalMetaForm.style.display = 'none';
+    }
+
+
+    function firstFormShow() {
+        // if (bmiForm.style.display === 'none') {
+        bmiForm.style.display = 'block';
+        caloriesForm.style.display = 'none';
+        basicMetaForm.style.display = 'none';
+        totalMetaForm.style.display = 'none';
+        //        } else {
+        //            bmiForm.display = 'none';
+        //        }
+    }
+
+    function secondFormShow() {
+        // if (bmiForm.style.display === 'none') {
+        bmiForm.style.display = 'none';
+        caloriesForm.style.display = 'block';
+        basicMetaForm.style.display = 'none';
+        totalMetaForm.style.display = 'none';
+        //        } else {
+        //            bmiForm.display = 'none';
+        //        }
+    }
+
+    function thirdFormShow() {
+        // if (bmiForm.style.display === 'none') {
+        bmiForm.style.display = 'none';
+        caloriesForm.style.display = 'none';
+        basicMetaForm.style.display = 'block';
+        totalMetaForm.style.display = 'none';
+        //        } else {
+        //            bmiForm.display = 'none';
+        //        }
+    }
+
+    function fourthFormShow() {
+        // if (bmiForm.style.display === 'none') {
+        bmiForm.style.display = 'none';
+        caloriesForm.style.display = 'none';
+        basicMetaForm.style.display = 'none';
+        totalMetaForm.style.display = 'block';
+        //        } else {
+        //            bmiForm.display = 'none';
+        //        }
+    }
+
+
+    firstMenuBtn.addEventListener('click', function () {
+        firstFormShow();
+        console.log('click');
+    });
+
+    secondMenuBtn.addEventListener('click', function () {
+        secondFormShow();
+        console.log('click');
+    });
+
+    thirdMenuBtn.addEventListener('click', function () {
+        thirdFormShow();
+        console.log('click');
+    });
+
+    fourthMenuBtn.addEventListener('click', function () {
+        fourthFormShow();
+        console.log('click');
+    });
+
+
+    init();
     
     
 });
+
+
+//function myFunction() {
+//    var x = document.getElementById('myDIV');
+//    if (x.style.display === 'none') {
+//        x.style.display = 'block';
+//    } else {
+//        x.style.display = 'none';
+//    }
+//}
